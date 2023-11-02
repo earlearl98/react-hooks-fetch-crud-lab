6053d2f3be716c95e00847b8985d338ac98e97a0
earlearl98 <earlearl1720@gmail.com>
2023-11-02 21:22:01 +0300

    Completed assignment

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,24 +1,32 @@
 import React from "react";
 
-function QuestionItem({ question }) {
-  const { id, prompt, answers, correctIndex } = question;
+function QuestionItem({ question ,onDeleteItem}) {
+  const {id,prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
       {answer}
     </option>
   ));
-
+  function handleDeleteClick() {
+    fetch(`http://localhost:4000/questions/${question.id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then(() => onDeleteItem(question));
+  }
   return (
+    <div className="QuestionItem">
     <li>
-      <h4>Question {id}</h4>
+      <h4>Question {question.id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
+    </div>
   );
 }
 
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,44 @@
-import React from "react";
+
+import React, { useState , useEffect} from "react";
+import QuestionForm from "./QuestionForm";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+  useEffect(() => {
+    // Make a GET request to the specified URL
+    fetch('http://localhost:4000/questions')
+    .then(response => response.json())  // Parse the JSON-formatted response
+    .then(result => setQuestions(result))    // Set the data in the state
+    .catch(error => console.error('Error:', error));  // Handle any errors
+    }, []);
+
+    console.log(questions)
+
+    function handleAddQuestion(newQuestion) {
+      setQuestions([...questions, newQuestion])
+    }
+
+
+    function handleDeleteQuestion(deletedQuestion) {
+      const updatedQuestions = questions.filter((question) => question.id !== deletedQuestion.id);
+      setQuestions(updatedQuestions);
+    }
+    
+
+
+
+  
+
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <QuestionForm onAddQuestion={handleAddQuestion} />
+      <QuestionItem key={questions.id} onDeleteItem={handleDeleteQuestion}/>
+      <ul>{questions.map(question => (
+<li key={question.id}>{question.prompt}</li>
+))}</ul>
     </section>
   );
 }
